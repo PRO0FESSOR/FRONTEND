33b712bb9717a069095a227dbc12b9630fec712d
PRO0FESSOR <siddharth96549@gmail.com>
2024-11-01 14:51:56 +0530

    primitive vs referrence type modified

--- javascript/learning/part1/file_28.js
+++ javascript/learning/part1/file_28.js
@@ -9,22 +9,28 @@ console.log(`num2 is ${num2}`);
 
 num1++;
 
-console.log(`num1 is ${num1}`);
-console.log(`num2 is ${num2}`);
 
-// in above example only num1 increases but num2 is same 6
+// intro to arrays 
+// reference type 
+// how to create arrays
 
-let array1 = ["item1", "item2"];
-let array2 = array1;
-console.log("array1", array1);
-console.log("array2", array2);
-array1.push("item3");
-console.log("after pushing element to array 1");
-console.log("array1", array1);
-console.log("array2", array2);
+// ordered collection of items 
 
-//in above example array2 also changes which makes them referrence type
+// let fruits = ["apple", "mango", "grapes"];
+// let numbers = [1,2,3,4];
+// let mixed = [1,2,2.3, "string", null, undefined];
+// console.log(mixed);
+// console.log(numbers);
+// console.log(fruits[2]);
+let fruits = ["apple", "mango", "grapes"];
+let obj = {}; // object literal
+// console.log(fruits);
+// fruits[1] = "banana";
+// console.log(fruits);
+console.log(typeof fruits);
+console.log(typeof obj);
+console.log(Array.isArray(fruits));
+console.log(Array.isArray(obj));
 
 
-// primitive data types are stored in the stack which requires only values 
-// referrence data types are stored in the heap and works using address
+// array indexing 
