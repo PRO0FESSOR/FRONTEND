e22698998c987b276f9f0245b37a248d8717a8b5
PRO0FESSOR <siddharth96549@gmail.com>
2024-12-23 20:36:15 +0530

    modified

--- javascript/learning/part1/file_85.js
+++ javascript/learning/part1/file_85.js
@@ -1,6 +1,6 @@
  let numbers = [1,2,3];
 
- // console.log(Object.getPrototypeOf(numbers));
+// console.log(Object.getPrototypeOf(numbers));
  console.log(Array.prototype);
  console.log(numbers);
 
