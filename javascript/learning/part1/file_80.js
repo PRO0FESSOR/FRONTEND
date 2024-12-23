f8ada420a40643c29512a2e618c7558f75d99a87
PRO0FESSOR <siddharth96549@gmail.com>
2024-12-23 19:42:56 +0530

    proto modified

--- javascript/learning/part1/file_80.js
+++ javascript/learning/part1/file_80.js
@@ -6,15 +6,13 @@ const obj1 = {
   key2 : "value2"
 }
 
-const obj2 = {
-  key3 : "value3"
-}
+//const obj2 = {
+//  key3 : "value3"
+//}
 
 console.log(obj1.key2);
-console.log(obj2.key2);
-
-// using object.create we set the proto property of obj3 which means now obj1 property are inherited by obj3
 
 const obj3 = Object.create(obj1);
 obj3.key3 = "value3";
+
 console.log(obj3.key1);
