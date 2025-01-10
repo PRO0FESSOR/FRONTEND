cf6c4001cbd65d31374f09877335c5bf37309acf
PRO0FESSOR <siddharth96549@gmail.com>
2025-01-10 16:38:08 +0530

    other types

---
+++ typescript/everydayTypes/file_03.ts
@@ -0,0 +1,17 @@
+
+// other types 
+
+enum action {
+  success = 1,
+    fail = 0,
+}
+
+// unknown type
+// better than any 
+
+let value: unknown;
+value = "alex";
+
+// any type - not safe
+
+let val: any = "alex";
