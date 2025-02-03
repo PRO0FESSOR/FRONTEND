bca1234b082ff0d51a81a233c80faf7bdb41a29b
PRO0FESSOR <siddharth96549@gmail.com>
2025-02-03 14:38:25 +0530

    type guard example

---
+++ typescript/narrowing/file_01.ts
@@ -0,0 +1,11 @@
+
+// type guards - for more type safety
+
+function padLeft(padding: number | string, input: string): string {
+  if (typeof padding === "number") {
+    return " ".repeat(padding) + input;
+  }
+  return padding + input;
+}
+
+
