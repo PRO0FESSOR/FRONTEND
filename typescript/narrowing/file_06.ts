0430a6c5f73cf401164222e8a22e3f337fad7ac9
PRO0FESSOR <siddharth96549@gmail.com>
2025-02-03 14:41:21 +0530

    in operator narrowing

---
+++ typescript/narrowing/file_06.ts
@@ -0,0 +1,15 @@
+
+// in operator narrowing 
+
+type Fish = { swim: () => void };
+type Bird = { fly: () => void };
+ 
+function move(animal: Fish | Bird) {
+  if ("swim" in animal) {
+    return animal.swim();
+  }
+ 
+  return animal.fly();
+}
+
+
