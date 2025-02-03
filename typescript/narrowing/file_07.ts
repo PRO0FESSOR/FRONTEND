d56bddeb52cd839100b26cf72befc195879a7ea4
PRO0FESSOR <siddharth96549@gmail.com>
2025-02-03 14:41:53 +0530

    instance of narrowing

---
+++ typescript/narrowing/file_07.ts
@@ -0,0 +1,16 @@
+
+//instance of narrowing 
+
+function logValue(x: Date | string) {
+  if (x instanceof Date) {
+    console.log(x.toUTCString());
+               
+(parameter) x: Date
+  } else {
+    console.log(x.toUpperCase());
+               
+(parameter) x: string
+  }
+}
+
+
