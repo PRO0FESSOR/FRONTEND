80e5c6878ba62b014bd356186a8abf513459e0c7
PRO0FESSOR <siddharth96549@gmail.com>
2025-02-03 14:42:51 +0530

    assignments

---
+++ typescript/narrowing/file_08.ts
@@ -0,0 +1,17 @@
+
+// assignments 
+
+let x = Math.random() < 0.5 ? 10 : "hello world!";
+   
+let x: string | number
+x = 1;
+ 
+console.log(x);
+           
+let x: number
+x = "goodbye!";
+ 
+console.log(x);
+           
+let x: string
+
