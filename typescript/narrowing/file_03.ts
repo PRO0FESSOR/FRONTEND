8e81cd8d01a39b7293500e595a7ba3c68539c4af
PRO0FESSOR <siddharth96549@gmail.com>
2025-02-03 14:39:27 +0530

    truthiness narrowing

---
+++ typescript/narrowing/file_03.ts
@@ -0,0 +1,9 @@
+
+// truthiness naroowing 
+
+function getUsersOnlineMessage(numUsersOnline: number) {
+  if (numUsersOnline) {
+    return `There are ${numUsersOnline} online now!`;
+  }
+  return "Nobody's here. :(";
+}
