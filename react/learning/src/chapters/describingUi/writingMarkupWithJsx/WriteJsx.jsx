009a78597266e16d7ebf521dd278df497222e58d
PRO0FESSOR <siddharth96549@gmail.com>
2025-02-04 20:55:30 +0530

    jsx

---
+++ react/learning/src/chapters/describingUi/writingMarkupWithJsx/WriteJsx.jsx
@@ -0,0 +1,11 @@
+
+const WriteJsx = () => {
+return (
+  <>
+    <p>This image is in jsx</p>
+    <img src='https://cdn4.iconfinder.com/data/icons/logos-3/600/React.js_logo-512.png' alt='Error' />
+  </>
+)
+}
+
+export default WriteJsx
